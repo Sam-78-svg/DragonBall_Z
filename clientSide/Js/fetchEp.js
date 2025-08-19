59545464042b266fdee8de3953d7e2fb923b49b6
Sam-78-svg <kendresangamwaman@gmail.com>
2025-08-20 00:32:16 +0530

    s2&3

--- clientSide/Js/fetchEp.js
+++ clientSide/Js/fetchEp.js
@@ -15,15 +15,19 @@ function fetchEp() {
 function display(data) {
     data.forEach(ep => {
         let displayEp = document.createElement('div');
-        displayEp.className = "p-3 mb-4 mt-4 rounded-4 row shadow-sm "
+        displayEp.className = "p-3 bg-light border border-4 border-danger-subtle mb-4 mt-2 rounded-4 row shadow-sm ";
+        displayEp.setAttribute = ('background-color', '#dab5a3')
         displayEp.innerHTML = `
-                            <div class="col-12 rounded-4 col-md-3">
+                            <div class="col-12 rounded-4 border border-2 p-0 col-md-3">
                                 <img src="${ep.image}"
-                                     style="object-fit:contain; height:100%;width:100%"
-                                     alt="${ep.episode_name}">
-                                </img>
+                                     style="object-fit:cover; height:100%;width:100%;cursor:pointer;"
+                                     alt="${ep.episode_name}"
+                                     class="rounded-4"
+                                     id="epImg"
+                                />
+        
                             </div>
-                            <div class="col-12 col-md-9 d-flex text-dark fs-5 flex-column border p-4 border-2" style="line-height:auto">
+                            <div class="col-12 col-md-9 d-flex text-dark fs-5 flex-column p-1 p-md-4" style="line-height:auto">
                                 <p><strong>Episode No. </strong>${ep.episode_number}</p>
                                 <p><strong>Name: </strong>${ep.episode_name}</p>
                                 <p><strong>Season: </strong>${ep.season_name}</p>
